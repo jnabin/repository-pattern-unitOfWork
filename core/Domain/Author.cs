7f812ed758f377d8c5f8ced535568ea886277d3b
nabin <jahangirnabin2@gmail.com>
2022-02-09 01:51:12 +0600

    first commit

---
+++ core/Domain/Author.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace core.Domain
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+
+        public Author()
+        {
+            Courses = new HashSet<Course>();
+        }
+
+    }
+}
