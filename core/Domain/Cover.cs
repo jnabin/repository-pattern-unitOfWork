f65aa71f0c6eb3e4faa58dd07fc4935b8e3bd2bd
nabin <jahangirnabin2@gmail.com>
2022-02-09 03:26:35 +0600

    adding migration

--- core/Domain/Cover.cs
+++ core/Domain/Cover.cs
@@ -9,6 +9,8 @@ namespace core.Domain
     public class Cover
     {
         public int Id { get; set; }
+
+        public int CourseId { get; set; }
         public Course Course { get; set; }
     }
 }
