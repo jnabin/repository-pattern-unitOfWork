4fc2710a5fcb119ba4e3ecff9336392583ceef15
nabin <jahangirnabin2@gmail.com>
2022-02-09 03:44:00 +0600

    adding persistence

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            new CourseConfiguration().Configure(builder.Entity<Course>());
+        }
+    }
+}
---
+++ Persistence/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(5000);
+
+            builder.HasKey(c => c.Id);
+
+            builder.HasOne(p => p.Author)
+                .WithMany(c => c.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(c => c.Cover)
+                .WithOne(i => i.Course)
+                .HasForeignKey<Cover>(c => c.CourseId);
+
+            builder.HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .UsingEntity(m => m.ToTable("CourseTags"));
+        }
+    }
+}
